fca3cbcf2025a775a7b644f546fb26c3b60a2918
Igor Cervac <igor.cervac@outlook.com>
2024-02-27 01:08:21 -0500

    Min/max using reduce

--- reduce/index.js
+++ reduce/index.js
@@ -1,3 +1,14 @@
-const cities = [{name: 'Montreal', population: 1762949}, {name: 'Quebec City', population: 549459}, {name: 'Sherbrooke', population: 172950}];
-const total = cities.reduce((x,y) => x+y.population, 0);
-console.info('Total population: ', total);
+const cities = [
+    {name: 'Montreal', population: 1762949}, 
+    {name: 'Quebec City', population: 549459}, 
+    {name: 'Sherbrooke', population: 172950}
+];
+
+const total = cities.reduce((total, city) => total + city.population, 0);
+console.info('Cities population: ', total);
+
+const minCity = cities.reduce((minCity, city) => city.population < minCity.population ? city : minCity);
+console.info('City with min. population: ', minCity.name, minCity.population);
+
+const maxCity = cities.reduce((maxCity, city) => city.population > maxCity.population ? city : maxCity);
+console.info('City with max. population: ', maxCity.name, maxCity.population);
