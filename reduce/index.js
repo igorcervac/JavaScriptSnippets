4954c5655054fd86c8e22cf6af2b04bffa87dffb
Igor Cervac <igor.cervac@outlook.com>
2024-04-22 05:48:52 -0400

    Refactor reduce changes

--- reduce/index.js
+++ reduce/index.js
@@ -1,23 +1,25 @@
-const cities = [
+const all_cities = [
     {name: 'Montreal', population: 1762949}, 
     {name: 'Quebec City', population: 549459}, 
-    {name: 'Sherbrooke', population: 172950}
+    {name: 'Sherbrooke', population: 172950},
+    {name:'Gatineau', population: 284557}
 ];
 
-const three_cities_population = cities.reduce((acc, city) => acc + city.population, 0);
-console.info('Three cities population: ', three_cities_population);
+const all_cities_population = all_cities.reduce((acc, city) => acc + city.population, 0);
+console.info('All cities population: ', all_cities_population);
 
-const populations = cities.map(c => c.population);
-const three_cities_population2 = populations.reduce((accumulator, value) => accumulator + value);
-console.info('Three cities population (no intial value): ', three_cities_population2);
+const all_cities_population2 = all_cities
+                            .map(c => c.population)
+                            .reduce((accumulator, value) => accumulator + value);
+console.info('All cities population (no intial value): ', all_cities_population2);
 
-const two_cities =  { length: 2 };
-cities.forEach((v, i) => two_cities[i] = v);
-const two_cities_population = Array.prototype.reduce.call(two_cities, (acc, city) => acc + city.population, 0);
-console.info('Two cities population: ', two_cities_population);
+const some_cities =  { length: 2 };
+all_cities.forEach((v, i) => some_cities[i] = v);
+const some_cities_population = Array.prototype.reduce.call(some_cities, (acc, city) => acc + city.population, 0);
+console.info('Some cities population: ', some_cities_population);
 
-const minCity = cities.reduce((minCity, city) => city.population < minCity.population ? city : minCity);
+const minCity = all_cities.reduce((minCity, city) => city.population < minCity.population ? city : minCity);
 console.info('City with min. population: ', minCity.name, minCity.population);
 
-const maxCity = cities.reduce((maxCity, city) => city.population > maxCity.population ? city : maxCity);
+const maxCity = all_cities.reduce((maxCity, city) => city.population > maxCity.population ? city : maxCity);
 console.info('City with max. population: ', maxCity.name, maxCity.population);
