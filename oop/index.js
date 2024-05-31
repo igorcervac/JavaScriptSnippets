6e6f6eb1aec8ca72a55b7e6838ee239b5dfbdacd
Igor Cervac <igor.cervac@outlook.com>
2024-05-31 04:49:01 -0400

    OOP: getter and setter

--- oop/index.js
+++ oop/index.js
@@ -8,12 +8,28 @@ function Certification(title, year, company){
         get: function(){
             return `${this.title} (${this.year}) by ${this.company}`;
         }
-    }); 
+    });
+    
+    Object.defineProperty(this, 'year', {
+        get: function(){
+            return year;
+        },
+        set: function(value){
+            if (value < 2000) {
+                throw new Error('Invalid year');
+            }
+            year = value;
+        }
+    })
     
     this.about = function(){
         console.log(this.description);
     }
 }
 
-var certification = new Certification('AI-102', 2024, 'Microsoft');
+var certification = new Certification('AI-102', 2023, 'Microsoft');
 certification.about();
+
+certification.year = 2024;
+certification.about();
+
