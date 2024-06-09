2171b095da51d09bffd07f37ad1b82b063636b84
Igor Cervac <igor.cervac@outlook.com>
2024-06-09 08:42:53 -0400

    Minor refactoring

--- oop/circle.js
+++ oop/circle.js
@@ -3,7 +3,7 @@ function Circle(){
 };
 
 Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle.constructor;
+Circle.prototype.constructor = Circle;
 Circle.prototype.draw = function(){
     console.log(this.name);
 };
